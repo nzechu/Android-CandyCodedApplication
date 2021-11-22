50cdf9360edb2c61e35b8033b35560ab6832497e
nzechu <arinzendubuishi@gmail.com>
2021-11-22 13:05:18 +0100

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,6 +43,16 @@ public class InfoActivity extends AppCompatActivity {
         }
 
     }
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent actionIntent = new Intent(Intent.ACTION_DIAL);
+        actionIntent.setData(uri);
+
+        startActivity(actionIntent);
+
+
+
+    }
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
