db3075192a7d5f0b8e8a4e5e6c3017e95a17fd55
nzechu <arinzendubuishi@gmail.com>
2021-11-22 13:40:45 +0100

    completed module

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -79,7 +79,7 @@ public class DetailActivity extends AppCompatActivity {
     private void createShareIntent(){
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("text/plain");
-                String shareString = "SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED";
+                String shareString = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
                 shareIntent.putExtra(Intent.EXTRA_TEXT,shareString);
 
                 startActivity(shareIntent);
